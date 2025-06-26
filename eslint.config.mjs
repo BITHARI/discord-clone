d8ea76c104b7b43e40c3ab32a2eb9bf671d144f1
BITHARI <yengayengavital@gmail.com>
2025-06-27 00:21:03 +0200

    Règles ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,43 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+    {
+        ignores: [
+            "node_modules",
+            "lib/generated/prisma",
+            ".next",
+            "public",
+            "dist",
+            "out",
+            "coverage",
+            "*.config.mjs",
+        ],
+    },
+
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+    {
+        rules: {
+            "react-hooks/exhaustive-deps": "off",
+            'allowObjectsTypes': 'off',
+            "no-useless-escape": "off",
+            "react/no-unescaped-entities": "off",
+            '@typescript-eslint/no-empty-object-type' : "off",
+            "@typescript-eslint/no-wrapper-object-types" : "off",
+            '@typescript-eslint/no-unnecessary-type-constraint': 'off',
+            "@typescript-eslint/no-explicit-any": "off",
+            '@next/next/no-img-element': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            '@typescript-eslint/no-require-imports': 'off',
+            '@typescript-eslint/no-unused-expressions': 'off',
+            '@typescript-eslint/no-this-alias': 'off',
+            '@typescript-eslint/no-unsafe-function-types': 'off',
+            '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+            
+        },
+    },
 ];
 
 export default eslintConfig;
+
